28034ee1d2bf33638d0f2f0880b8dcfcde095593
Nightmare1984 <gunther6000@gmail.com>
2025-05-23 04:19:05 -0400

    Adding ArtixLinux Support for System Updater

--- core/tabs/system-setup/system-update.sh
+++ core/tabs/system-setup/system-update.sh
@@ -2,32 +2,16 @@
 
 . ../common-script.sh
 
-fastUpdate() {
-    printf "%b\n" "${YELLOW}Automatically updating Arch Linux mirrors for your area.${RC}"
-    if command_exists reflector; then
-        "$ESCALATION_TOOL" reflector --country "$(curl -s https://ipapi.co/country/)" --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
-        printf "%b\n" "${GREEN}Mirrors updated successfully for your area.${RC}"
-    else
-        printf "%b\n" "${RED}Reflector is not installed. Please install it to update mirrors.${RC}"
-        exit 1
-    fi
-    printf "%b\n" "${YELLOW}Updating AUR helper mirrors.${RC}"
-    if [ "$AUR_HELPER" = "paru" ] || [ "$AUR_HELPER" = "yay" ]; then
-        "$AUR_HELPER" -Y --gendb
-        "$AUR_HELPER" -Y --topdown
-        printf "%b\n" "${GREEN}AUR helper mirrors updated successfully.${RC}"
-    else
-        printf "%b\n" "${RED}Unsupported AUR helper: ${AUR_HELPER}${RC}"
-        exit 1
-    fi
-}
-
 updateSystem() {
     printf "%b\n" "${YELLOW}Updating system packages.${RC}"
     case "$PACKAGER" in
         pacman)
-            "$ESCALATION_TOOL" "$PACKAGER" -Sy --noconfirm --needed archlinux-keyring
-            "$AUR_HELPER" -Su --noconfirm
+            if [ -f /etc/artix-release ]; then
+                "$ESCALATION_TOOL" "$PACKAGER" -Sy --noconfirm --needed artix-keyring
+            else
+                "$ESCALATION_TOOL" "$PACKAGER" -Sy --noconfirm --needed archlinux-keyring
+            fi
+                "$AUR_HELPER" -Su --noconfirm
             ;;
         *)
             printf "%b\n" "${RED}Unsupported package manager: ${PACKAGER}${RC}"
@@ -46,6 +30,5 @@ updateFlatpaks() {
 checkEnv
 checkAURHelper
 checkEscalationTool
-fastUpdate
 updateSystem
 updateFlatpaks
